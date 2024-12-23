92ad6aa772ea9696511e7d96e63d05db254a4d02
Richard Watts <richard@zilliqa.com>
2024-12-23 16:13:37 +0000

    (fix) You should never use the Zilliqa mainnet unrestricted lockproxyproxy, since the Zilliqa LockProxy isn't EVM (fix) Mark the Zilliqa LockAndReleaseTokenManager as not to use , since if you try to get it to lock native tokens, it won't.

--- smart-contracts/script/mainnetConfig.s.sol
+++ smart-contracts/script/mainnetConfig.s.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.8.20;
  * chains that I haven't,  yet.
  */
 abstract contract MainnetConfig {
+  uint public constant zilChainId = 32769;
+  uint public constant bscChainId = 56;
+  uint public constant polChainId = 137;
+  uint public constant arbChainId = 42161;
+  uint public constant ethChainId = 1;
+
   address public constant zilLockProxy = 0xd73C6b871b4D0E130d64581993B745FC938A5be7;
-  address public constant zilUnrestrictedLockProxyProxy = 0x7519550ae8b6f9d32E9c1A939Fb5C186f660BE5b;
+  // This exists, but is useless, because you can't set extensions on Scilla lockProxy.
+  // address public constant zilUnrestrictedLockProxyProxy = 0x7519550ae8b6f9d32E9c1A939Fb5C186f660BE5b;
   address public constant zilChainGatewayAddress = 0xbA44BC29371E19117DA666B729A1c6e1b35DDb40;
-  address public constant zilLockAndReleaseTokenManager = 0x6D61eFb60C17979816E4cE12CD5D29054E755948;
+  address public constant zilLockAndReleaseTokenManagerDoNotUseForZilbridge = 0x6D61eFb60C17979816E4cE12CD5D29054E755948;
 
   address public constant zilLockAndReleaseOrNativeTokenManagerUpgradeable = 0x4fa6148C9DAbC7A737422fb1b3AB9088c878d26C;
   address public constant zilLockProxyTokenManager = 0xb4132E757345f0EfD91af38cD824Be441F34fe25;
