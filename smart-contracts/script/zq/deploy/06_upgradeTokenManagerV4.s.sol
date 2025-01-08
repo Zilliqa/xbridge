3481f78c7773d8e6bbf3ccfdb030fd525cf81781
Richard Watts <richard@zilliqa.com>
2025-01-08 20:55:33 +0000

    (feat) Zilliqa side token manager upgrade script

---
+++ smart-contracts/script/zq/deploy/06_upgradeTokenManagerV4.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {TokenManagerUpgradeable} from "contracts/periphery/TokenManagerUpgradeable.sol";
+import {LockAndReleaseOrNativeTokenManagerUpgradeableV4} from "contracts/periphery/TokenManagerV4/LockAndReleaseOrNativeTokenManagerUpgradeableV4.sol";
+import {LockProxyTokenManagerUpgradeableV4} from "contracts/periphery/TokenManagerV4/LockProxyTokenManagerUpgradeableV4.sol";
+import "forge-std/console.sol";
+import { MainnetConfig } from "script/mainnetConfig.s.sol";
+import { TokenManagerDeployerUtil } from "script/tokenManagerDeployerUtil.s.sol";
+
+
+contract Upgrade is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_OWNER");
+        address owner = vm.addr(deployerPrivateKey);
+        console.log("Signer is %s", owner);
+
+        // Constants
+        {
+          TokenManagerUpgradeable tokenManager = TokenManagerUpgradeable(zilLockAndReleaseOrNativeTokenManagerUpgradeable);
+          vm.startBroadcast(deployerPrivateKey);
+          address newImplementation = address(new LockAndReleaseOrNativeTokenManagerUpgradeableV4());
+          bytes memory encodedReinitializerCall = "";
+          tokenManager.upgradeToAndCall(
+              newImplementation,
+              encodedReinitializerCall);
+          LockAndReleaseOrNativeTokenManagerUpgradeableV4 tokenManagerV4 = LockAndReleaseOrNativeTokenManagerUpgradeableV4(tokenManagerAddress);
+          console.log("New pending owner: %s", tokenManagerV4.pendingOwner());
+          console.log("LockAndRelease new implementation: %s", address(newImplementation));
+          vm.stopBroadcast();
+        }
+    }
+}
